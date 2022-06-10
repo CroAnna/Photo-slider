d4cfd8d2204759343b283abbcb425a87cd166e37
Ana Skarica <ana.skarica.as@gmail.com>
2022-06-10 23:49:45 +0200

    slozeno pomicanje diva sa slikama na gumb

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-"use strict";
+$(document).ready(function () {
+  $("#left").click(function () {
+    $(".slike").animate({ left: "+=175px" });
+  });
+
+  $("#right").click(function () {
+    $(".slike").animate({ left: "-=175px" });
+  });
+});
