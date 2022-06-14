88de459227aa8de6149aa105eddd55808a6a74a9
Ana Skarica <ana.skarica.as@gmail.com>
2022-06-14 19:38:49 +0200

    pri kliku na strelicu se na kratko promijeni slika (tj tip strelice / sliva ili plava)

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+let brojac = 0;
+
 $(document).ready(function () {
   $("#left").click(function () {
     $(".slike").animate({ left: "+=175px" });
+    brojac--;
+    console.log(brojac);
+    $(".arrow-left").attr("src", "Assets/arrow-gray-left.png");
+    odgoda("left");
   });
 
   $("#right").click(function () {
     $(".slike").animate({ left: "-=175px" });
+    brojac++;
+    $(".arrow-right").attr("src", "Assets/arrow-gray-right.png");
+    odgoda("right");
   });
 });
+
+function odgoda(strana) {
+  setTimeout(function () {
+    $(`.arrow-${strana}`).attr("src", `Assets/arrow-blue-${strana}.png`);
+  }, 300);
+}
