5bb7f1d62ae239f8e961d36dfa9931f91f267480
Ana Skarica <ana.skarica.as@gmail.com>
2022-06-21 15:41:48 +0200

    dodan header, footer, tekst na engleski

--- script.js
+++ script.js
@@ -1,84 +1,86 @@
-let brojac = 0;
-let ukupnaSirina = 0;
-
 $(document).ready(function () {
-  sakrijPocetno();
-  $("#left").click(klikLijevo);
-  $("#right").click(klikDesno);
+  let counter = 0;
+  let totalWidth = 0;
+  hideStart();
+  $("#left").click(clickLeft);
+  $("#right").click(clickRight);
+
+  function clickLeft() {
+    if (counter <= 0) {
+      totalWidth = 0;
+      for (let i = 1; i <= 7; i++) {
+        totalWidth += $(`.sl${i}`).width();
+      }
+      $(".images").animate({ left: `+=${$(`.sl8`).width() + 30}px` });
+      movementTimeout("left");
+      counter = 7;
+      return;
+    }
+
+    let widthMovement = $(`.sl${counter}`).width() + 30;
+    $(".images").animate({ left: `+=${widthMovement}px` });
+    counter--;
+    $(".arrow-left").attr("src", "Assets/arrow-gray-left.png");
+    arrowTimeout("left");
+  }
+
+  function clickRight() {
+    if (counter >= 7) {
+      totalWidth = 0;
+      for (let i = 2; i <= 8; i++) {
+        totalWidth += $(`.sl${i}`).width();
+      }
+      $(".images").animate({ left: `-=${$(`.sl8`).width() + 30}px` });
+      movementTimeout("right");
+      counter = 0;
+      return;
+    }
+    let widthMovement = $(`.sl${counter + 1}`).width() + 30;
+    $(".images").animate({ left: `-=${widthMovement}px` });
+    counter++;
+    $(".arrow-right").attr("src", "Assets/arrow-gray-right.png");
+    arrowTimeout("right");
+  }
+
+  function arrowTimeout(direction) {
+    setTimeout(function () {
+      $(`.arrow-${direction}`).attr(
+        "src",
+        `Assets/arrow-blue-${direction}.png`
+      );
+    }, 300);
+  }
+
+  function movementTimeout(direction) {
+    if (direction == "right") {
+      setTimeout(function () {
+        $(".images").css({
+          left:
+            $(".images").position().left +
+            totalWidth +
+            150 +
+            $(`.sl1`).width() +
+            "px",
+        });
+      }, 500);
+    } else {
+      setTimeout(function () {
+        $(".images").css({
+          left:
+            $(".images").position().left -
+            totalWidth +
+            //-200 -
+            -180 -
+            $(`.sl1`).width() +
+            "px",
+        });
+      }, 500);
+    }
+  }
+
+  function hideStart() {
+    $(".images").css({
+      right: $(`.sl8`).width() + $(`.sl7`).width() + 60 + "px",
+    });
+  }
 });
-
-function klikLijevo() {
-  if (brojac <= 0) {
-    ukupnaSirina = 0;
-    for (let i = 1; i <= 7; i++) {
-      ukupnaSirina += $(`.sl${i}`).width();
-    }
-    $(".slike").animate({ left: `+=${$(`.sl8`).width() + 30}px` });
-    odgodaPomicanja("lijevo");
-    brojac = 7;
-    return;
-  }
-
-  let sirinaZaPomak = $(`.sl${brojac}`).width() + 30;
-  $(".slike").animate({ left: `+=${sirinaZaPomak}px` });
-  brojac--;
-  $(".arrow-left").attr("src", "Assets/arrow-gray-left.png");
-  odgodaStrelice("left");
-}
-
-function klikDesno() {
-  if (brojac >= 7) {
-    ukupnaSirina = 0;
-    for (let i = 2; i <= 8; i++) {
-      ukupnaSirina += $(`.sl${i}`).width();
-    }
-    $(".slike").animate({ left: `-=${$(`.sl8`).width() + 30}px` });
-    odgodaPomicanja("desno");
-    brojac = 0;
-    return;
-  }
-  let sirinaZaPomak = $(`.sl${brojac + 1}`).width() + 30;
-  $(".slike").animate({ left: `-=${sirinaZaPomak}px` });
-  brojac++;
-  $(".arrow-right").attr("src", "Assets/arrow-gray-right.png");
-  odgodaStrelice("right");
-}
-
-function odgodaStrelice(smjer) {
-  setTimeout(function () {
-    $(`.arrow-${smjer}`).attr("src", `Assets/arrow-blue-${smjer}.png`);
-  }, 300);
-}
-
-function odgodaPomicanja(smjer) {
-  if (smjer == "desno") {
-    setTimeout(function () {
-      $(".slike").css({
-        left:
-          $(".slike").position().left +
-          ukupnaSirina +
-          150 +
-          $(`.sl1`).width() +
-          "px",
-      });
-    }, 500);
-  } else {
-    setTimeout(function () {
-      $(".slike").css({
-        left:
-          $(".slike").position().left -
-          ukupnaSirina +
-          //-200 -
-          -180 -
-          $(`.sl1`).width() +
-          "px",
-      });
-    }, 500);
-  }
-}
-
-function sakrijPocetno() {
-  $(".slike").css({
-    right: $(`.sl8`).width() + $(`.sl7`).width() + 60 + "px",
-  });
-}
